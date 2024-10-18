8810b53338b12c3ec5e5604d38dd289d669edb14
Msithiphong <Csithiphonggg@gmail.com>
2024-10-17 20:03:39 -0700

    Update generic.hs
    Modified to fit rubric + added documentation

--- generic.hs
+++ generic.hs
@@ -1,5 +1,5 @@
-import Data.List (sort, sortBy)
-import Data.Ord (comparing)
+import Data.List 
+import Data.Ord 
 
 numbers :: [Double]
 numbers = [645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25]
@@ -10,17 +10,31 @@ people = [("Hal", 20), ("Susann", 31), ("Dwight", 19), ("Kassandra", 21),
           ("Doretha", 32), ("Danna", 20), ("Zara", 23), ("Rosalyn", 26), ("Risa", 24), 
           ("Benny", 28), ("Juan", 33), ("Natalie", 25)]
 
+-- generic sort function
+generic :: (a -> a -> Ordering) -> [a] -> [a]
+generic = sortBy
 
+
+-- create specialized sort functions using
+-- generic sort function for numbers, names and ages
 sortNumbers :: [Double] -> [Double]
-sortNumbers = sort
+sortNumbers = generic compare
 
 sortPeople :: [(String, Int)] -> [(String, Int)]
-sortPeople = sort
+sortPeople = generic compare
 
 sortAge :: [(String, Int)] -> [(String, Int)]
-sortAge = sortBy (comparing (negate . snd) <> comparing fst)
+sortAge = generic (comparing (negate . snd) <> comparing fst)
 
-main = do
+-- comparing (negate . snd) sorts the negated ages
+-- to achieve descending age order
+-- if ages of two people are (EQ)
+-- then compare fst using mappend operator
+-- mappend operator combines the 2 comparisons
+
+
+-- print sort results
+genericSort = do
 
     let sortNums = sortNumbers numbers
     print sortNums
